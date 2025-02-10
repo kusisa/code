d3a7bf42c0f5cf8594f59dbfc6e925e69fea35fa
kusisa <2942855457@qq.com>
2025-02-10 11:38:45 +0800

    first

---
+++ point.h
@@ -0,0 +1,17 @@
+#pragma once 
+#include<iostream>
+
+using namespace std;
+class point 
+{
+  public:
+  void setx(int x);
+  int getx();
+  
+  void sety(int y);
+  int gety();
+  
+  private:
+  int m_x;
+  int m_y;
+};
